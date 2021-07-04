818445d74cdc9e265378d2567d671eae5e0c47dc
yuki ueno <yuki.weaver1204@gmail.com>
2021-07-04 16:24:25 +0900

    ユーザ登録 不具合修正

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'nickname', 'email', 'password',
+        'nickname', 'email', 'password', 'monthly_start_date',
     ];
 
     /**
